83f346f2b2f19cecd8f31f226972a84c81b4ab85
Omer Golan Joel <golan2072@gmail.com>
2021-11-19 21:55:06 +0200

    0.9

--- main.py
+++ main.py
@@ -17,6 +17,12 @@ class Book_product:
         self.cost = 0
         self.release_month = 0
         self.release_year = 0
+        # royalties
+        self.richard = 0
+        self.josh = 0
+        self.bob = 0
+        self.hannah = 0
+        self.ivan = 0
 
 
 class Data_item:
@@ -32,7 +38,7 @@ def month_calculation(start_month, start_year, end_month, end_year):
     year_difference = int(end_year) - int(start_year)
     if year_difference < 0:
         year_difference = 0
-    return year_difference*12 + int(end_month-start_month)
+    return year_difference * 12 + int(end_month - start_month)
 
 
 def product_lister(dict_list):
@@ -62,25 +68,40 @@ def product_cleaner(product_dictionary):
             output_product_dictionary[product] = product_dictionary[product]
         else:
             pass
-    output_product_dictionary["TSAO: Wreck in the Ring"] = merge_products(product_dictionary["TSAO: Wreck in the Ring"], product_dictionary["Borderlands Adventure 1: Wreck in the Ring"])
+    output_product_dictionary["TSAO: Wreck in the Ring"] = merge_products(product_dictionary["TSAO: Wreck in the Ring"],
+                                                                          product_dictionary[
+                                                                              "Borderlands Adventure 1: Wreck in the Ring"])
     output_product_dictionary.pop("Borderlands Adventure 1: Wreck in the Ring")
-    output_product_dictionary["Character Options for Stars Without Number"] = merge_products(product_dictionary["Character Options for Stars Without Number"], product_dictionary["Character Options - compatible with Stars Without Number"])
+    output_product_dictionary["Character Options for Stars Without Number"] = merge_products(
+        product_dictionary["Character Options for Stars Without Number"],
+        product_dictionary["Character Options - compatible with Stars Without Number"])
     output_product_dictionary.pop("Character Options - compatible with Stars Without Number")
-    output_product_dictionary["Cheating Death 2nd Edition"] = merge_products(product_dictionary["Cheating Death 2nd Edition"], product_dictionary["Cheating Death"])
+    output_product_dictionary["Cheating Death 2nd Edition"] = merge_products(
+        product_dictionary["Cheating Death 2nd Edition"], product_dictionary["Cheating Death"])
     output_product_dictionary.pop("Cheating Death")
-    output_product_dictionary["From the Ashes 2nd Edition"] = merge_products(product_dictionary["From the Ashes 2nd Edition"], product_dictionary["From the Ashes"])
+    output_product_dictionary["From the Ashes 2nd Edition"] = merge_products(
+        product_dictionary["From the Ashes 2nd Edition"], product_dictionary["From the Ashes"])
     output_product_dictionary.pop("From the Ashes")
-    output_product_dictionary["TSAO: Stationery and Heraldry Pack"] = merge_products(product_dictionary["TSAO: Stationery and Heraldry Pack"], product_dictionary["TSAO: Stationary and Heraldry Pack"])
+    output_product_dictionary["TSAO: Stationery and Heraldry Pack"] = merge_products(
+        product_dictionary["TSAO: Stationery and Heraldry Pack"],
+        product_dictionary["TSAO: Stationary and Heraldry Pack"])
     output_product_dictionary.pop("TSAO: Stationary and Heraldry Pack")
-    output_product_dictionary["TSAO: Liberty Ship"] = merge_products(product_dictionary["TSAO: Liberty Ship"], product_dictionary["Liberty Ship"])
+    output_product_dictionary["TSAO: Liberty Ship"] = merge_products(product_dictionary["TSAO: Liberty Ship"],
+                                                                     product_dictionary["Liberty Ship"])
     output_product_dictionary.pop("Liberty Ship")
-    output_product_dictionary["TSAO: 50 Wonders of the Reticulan Empire"] = merge_products(product_dictionary["TSAO: 50 Wonders of the Reticulan Empire"], product_dictionary["50 Wonders of the Reticulan Empire"])
+    output_product_dictionary["TSAO: 50 Wonders of the Reticulan Empire"] = merge_products(
+        product_dictionary["TSAO: 50 Wonders of the Reticulan Empire"],
+        product_dictionary["50 Wonders of the Reticulan Empire"])
     output_product_dictionary.pop("50 Wonders of the Reticulan Empire")
-    output_product_dictionary["The Sword of Cepheus"] = merge_products(product_dictionary["The Sword of Cepheus"], product_dictionary["Sword of Cepheus"])
+    output_product_dictionary["The Sword of Cepheus"] = merge_products(product_dictionary["The Sword of Cepheus"],
+                                                                       product_dictionary["Sword of Cepheus"])
     output_product_dictionary.pop("Sword of Cepheus")
-    output_product_dictionary["TSAO: Wreck in the Ring"] = merge_products(product_dictionary["TSAO: Wreck in the Ring"], product_dictionary["TSAO: Borderlands Adventure 1: Wreck in the Ring"])
+    output_product_dictionary["TSAO: Wreck in the Ring"] = merge_products(product_dictionary["TSAO: Wreck in the Ring"],
+                                                                          product_dictionary[
+                                                                              "TSAO: Borderlands Adventure 1: Wreck in the Ring"])
     output_product_dictionary.pop("TSAO: Borderlands Adventure 1: Wreck in the Ring")
-    output_product_dictionary["TSAO: These Stars Are Ours!"] = merge_products(product_dictionary["TSAO: These Stars Are Ours!"], product_dictionary["These Stars Are Ours!"])
+    output_product_dictionary["TSAO: These Stars Are Ours!"] = merge_products(
+        product_dictionary["TSAO: These Stars Are Ours!"], product_dictionary["These Stars Are Ours!"])
     output_product_dictionary.pop("These Stars Are Ours!")
     output_product_dictionary["Barbaro!"] = output_product_dictionary["Â¡BÃ¡rbaro!"]
     output_product_dictionary.pop("Â¡BÃ¡rbaro!")
@@ -94,7 +115,8 @@ def excel_output(product_dictionary):
     sheet.title = "Monthly Output"
     sheet['A1'] = "Monthly Output"
     sheet['A1'].font = Font(size=16, bold=True)
-    for cell in ["A3", "B3", "C3", "D3", "E3", "F3", "G3", "H3", "I3", "J3"]:
+    for cell in ["A3", "B3", "C3", "D3", "E3", "F3", "G3", "H3", "I3", "J3", "K3", "L3", "M3", "N3", "A60", "F60",
+                 "G60", "N60", "O3", "P3", "Q3", "R3", "S3", "O60", "P60", "Q60", "R60", "S60"]:
         sheet[cell].font = Font(bold=True)
     sheet['A3'] = "Product"
     sheet['B3'] = "Author"
@@ -106,13 +128,40 @@ def excel_output(product_dictionary):
     sheet['H3'] = "Release Year"
     sheet['I3'] = "Release Month"
     sheet['J3'] = "Months Since Release"
+    sheet['K3'] = "Net Profit"
+    sheet['L3'] = "Avg. Monthly Sales"
+    sheet['M3'] = "Avg. Monthly Profit"
+    sheet['N3'] = "Owner's Share"
+    sheet['O3'] = "Richard's Royalties"
+    sheet['P3'] = "Josh's Royalties"
+    sheet['Q3'] = "Bob's Royalties"
+    sheet['R3'] = "Hannah's Royalties"
+    sheet['S3'] = "Ivan's Royalties"
+    sheet['A60'] = "Total"
+    sheet["F60"] = "=SUM(F4:F59)"
+    sheet["G60"] = "=SUM(G4:G59)"
+    sheet["N60"] = "=SUM(N4:N59)"
+    sheet["O60"] = "=SUM(O4:O59)"
+    sheet["P60"] = "=SUM(P4:P59)"
+    sheet["Q60"] = "=SUM(Q4:Q59)"
+    sheet["R60"] = "=SUM(R4:R59)"
+    sheet["S60"] = "=SUM(S4:S59)"
     for row in sheet.iter_rows(min_row=2, max_row=1000):
         for column in sheet.iter_cols(min_row=2, max_row=150, min_col=6, max_col=7):
             for cell in column:
-                cell.number_format = '0.00'
+                cell.number_format = '$#,##0.00'
         for column in sheet.iter_cols(min_row=2, max_row=150, min_col=3, max_col=3):
             for cell in column:
-                cell.number_format = '0.00'
+                cell.number_format = '$#,##0.00'
+    for column in sheet.iter_cols(min_row=2, max_row=150, min_col=11, max_col=11):
+        for cell in column:
+            cell.number_format = '$#,##0.00'
+    for column in sheet.iter_cols(min_row=2, max_row=150, min_col=12, max_col=12):
+        for cell in column:
+            cell.number_format = '0.00'
+    for column in sheet.iter_cols(min_row=2, max_row=150, min_col=13, max_col=19):
+        for cell in column:
+            cell.number_format = '$#,##0.00'
     sheet.column_dimensions['A'].width = 52
     sheet.column_dimensions['B'].width = 32
     sheet.column_dimensions['C'].width = 20
@@ -123,9 +172,48 @@ def excel_output(product_dictionary):
     sheet.column_dimensions['H'].width = 20
     sheet.column_dimensions['I'].width = 20
     sheet.column_dimensions['J'].width = 20
+    sheet.column_dimensions['K'].width = 20
+    sheet.column_dimensions['L'].width = 20
+    sheet.column_dimensions['M'].width = 20
+    sheet.column_dimensions['N'].width = 20
+    sheet.column_dimensions['O'].width = 20
+    sheet.column_dimensions['P'].width = 20
+    sheet.column_dimensions['Q'].width = 20
+    sheet.column_dimensions['R'].width = 20
+    sheet.column_dimensions['S'].width = 20
     excel_row = 3
     for product in product_dictionary:
         excel_row += 1
+        profit = float(float(product_dictionary[product].total_revenue) - float(product_dictionary[product].cost))
+        month_differential = month_calculation(int(product_dictionary[product].release_month),
+                                               int(product_dictionary[product].release_year), 11, 2021)
+        if month_differential > 0:
+            monthly_sales = product_dictionary[product].total_count / month_differential
+            if profit < 0:
+                monthly_profit = 0
+            else:
+                monthly_profit = profit / month_differential
+        else:
+            monthly_sales = product_dictionary[product].total_count
+            monthly_profit = profit
+            if profit < 0:
+                monthly_profit = 0
+            else:
+                monthly_profit = profit
+        if profit > 0:
+            owners_share = 0.25 * product_dictionary[product].monthly_revenue
+            richard_royalties = float(product_dictionary[product].richard) * float(product_dictionary[product].monthly_revenue)
+            josh_royalties = product_dictionary[product].josh * product_dictionary[product].monthly_revenue
+            bob_royalties = product_dictionary[product].bob * product_dictionary[product].monthly_revenue
+            hannah_royalties = product_dictionary[product].hannah * product_dictionary[product].monthly_revenue
+            ivan_royalties = product_dictionary[product].ivan * product_dictionary[product].monthly_revenue
+        else:
+            owners_share = 0
+            richard_royalties = 0
+            josh_royalties = 0
+            bob_royalties = 0
+            hannah_royalties = 0
+            ivan_royalties = 0
         sheet.cell(row=excel_row, column=1).value = str(product_dictionary[product].name)
         sheet.cell(row=excel_row, column=2).value = str(product_dictionary[product].author)
         sheet.cell(row=excel_row, column=3).value = float(product_dictionary[product].cost)
@@ -133,9 +221,18 @@ def excel_output(product_dictionary):
         sheet.cell(row=excel_row, column=5).value = float(product_dictionary[product].monthly_count)
         sheet.cell(row=excel_row, column=6).value = float(product_dictionary[product].total_revenue)
         sheet.cell(row=excel_row, column=7).value = float(product_dictionary[product].monthly_revenue)
-        sheet.cell(row=excel_row, column=8).value = float(product_dictionary[product].release_year)
-        sheet.cell(row=excel_row, column=9).value = float(product_dictionary[product].release_month)
-        sheet.cell(row=excel_row, column=10).value = month_calculation(int(product_dictionary[product].release_month), int(product_dictionary[product].release_year), 11, 2021)
+        sheet.cell(row=excel_row, column=8).value = int(product_dictionary[product].release_year)
+        sheet.cell(row=excel_row, column=9).value = int(product_dictionary[product].release_month)
+        sheet.cell(row=excel_row, column=10).value = month_differential
+        sheet.cell(row=excel_row, column=11).value = profit
+        sheet.cell(row=excel_row, column=12).value = monthly_sales
+        sheet.cell(row=excel_row, column=13).value = monthly_profit
+        sheet.cell(row=excel_row, column=14).value = owners_share
+        sheet.cell(row=excel_row, column=15).value = richard_royalties
+        sheet.cell(row=excel_row, column=16).value = josh_royalties
+        sheet.cell(row=excel_row, column=17).value = bob_royalties
+        sheet.cell(row=excel_row, column=18).value = hannah_royalties
+        sheet.cell(row=excel_row, column=19).value = ivan_royalties
     workbook.save(filename)
 
 
@@ -153,7 +250,7 @@ if __name__ == '__main__':
             if product_dict["Name"] == product:
                 product_dictionary[product].total_revenue += float(product_dict["Earnings"])
                 product_dictionary[product].total_count += int(product_dict["Quantity"])
-                if product_dict["Date"][0:7] == "2021-10" or product_dict["Date"][3:10] == "10/2021":
+                if product_dict["Date"][0:7] == "2021-11" or product_dict["Date"][3:10] == "11/2021":
                     product_dictionary[product].monthly_revenue += float(product_dict["Earnings"])
                     product_dictionary[product].monthly_count += int(product_dict["Quantity"])
                 if product_dict["Earnings"] == "0":
@@ -173,6 +270,12 @@ if __name__ == '__main__':
                 product_dictionary[line["ï»¿Product"]].author = line["Author"]
                 product_dictionary[line["ï»¿Product"]].release_year = line["Release_Year"]
                 product_dictionary[line["ï»¿Product"]].release_month = line["Release_Month"]
+                product_dictionary[line["ï»¿Product"]].richard = float(line["Richard"])
+                product_dictionary[line["ï»¿Product"]].josh = float(line["Josh"])
+                product_dictionary[line["ï»¿Product"]].bob = float(line["Bob"])
+                product_dictionary[line["ï»¿Product"]].hannah = float(line["Hannah"])
+                product_dictionary[line["ï»¿Product"]].ivan = float(line["Ivan"])
             else:
                 pass
+    print("Processing...")
     excel_output(product_dictionary)
